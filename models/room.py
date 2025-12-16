47876721bec22081ff3ed3dff34e9ae938155594
jeong-gu <songg9572@gmail.com>
2025-12-17 03:25:50 +0900

    refactor: remove unused Room model

--- models/room.py
+++
@@ -1,8 +0,0 @@
-# models/room.py
-
-class Room:
-    def __init__(self, id: int, name: str, capacity: int, location: str):
-        self.id = id
-        self.name = name
-        self.capacity = capacity
-        self.location = location
