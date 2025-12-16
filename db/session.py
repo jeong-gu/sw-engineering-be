62cd0a9d83c49b2e1301b45d2ba9bdf4ffad296c
jeong-gu <songg9572@gmail.com>
2025-12-17 04:26:14 +0900

    feat: apply get_db dependency for database operations

--- db/session.py
+++ db/session.py
@@ -1,5 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from typing import Generator
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 DATABASE_URL = "sqlite:///./reservations.db"
 
@@ -13,3 +23,4 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
